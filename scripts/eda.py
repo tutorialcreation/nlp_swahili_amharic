a8ee45c471905bf9e682ec22ab520ee8f592726d
tutorialcreation <tutorialcreation@gmail.com>
2022-05-17 06:18:28 +0300

    added code for EDA analysis

--- scripts/eda.py
+++ scripts/eda.py
@@ -31,6 +31,46 @@ class EDA:
             summary=self.df.shape
         return summary
 
-    
+    def has_missing_values(self):
+        """
+        expects:
+            -   nothing
+        returns:
+            -   boolean
+        """
+        has_missing_values = False
+        if True in self.df.isnull().any().to_list():
+            has_missing_values=True
+        return has_missing_values
 
+    def plot_counts(self,column,second_column=None,type=None):
+        """
+        expects:
+            -   string
+        returns:
+            -   plot
+        """
+        fig,ax = plt.subplots()
+        if type == "univariate":
+            sns.countplot(data=self.df, x=column)
+            plt.title(f'Unique value counts of the {column} column');
+            plt.show()
+        elif type == "bivariate":
+            sns.countplot(data=self.df, x=second_column, hue=column)
+            plt.title(f'{column} vs {second_column}')
+            plt.show()
+        return fig
     
+    def correlation_analysis(self):
+        """
+        expects:
+            - nothing
+        returns:
+            - dataframe
+        """
+        corr = self.df.corr()
+        fig,ax=plt.subplots()
+        sns.heatmap(corr, annot=True)
+        plt.title('Heatmap of correlation for the numerical columns')
+        plt.show()
+        return fig
