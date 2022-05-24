1200936548cb9e2e06c79d00ed40bfc72e5ee49d
tutorialcreation <tutorialcreation81@gmail.com>
2022-05-24 18:17:00 +0300

    initialized the model and performed the adfuller test

--- scripts/timeseries.py
+++ scripts/timeseries.py
@@ -3,6 +3,14 @@ import numpy as np
 import pandas as pd
 import warnings
 import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+from matplotlib import ticker
+from statsmodels.tsa.stattools import adfuller, acf, pacf
+from sklearn.preprocessing import MinMaxScaler
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import LSTM
 from pandas.plotting import scatter_matrix
 import os
 print(os.getcwd())
@@ -28,6 +36,14 @@ class TimeSeries:
         """returns the df"""
         return self.df
 
+    def perform_adfuller(self,column):
+        """
+        - this algorithm performs the adfuller test
+        """
+        result = adfuller(self.df[column].values, autolag='AIC')
+        if result:
+            logger.info("succesfully performed adfuller test")
+        return result
 
 if __name__=='__main__':
     train_path = sys.argv[1]
