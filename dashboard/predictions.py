2f621c09f65e5bd8f261b7173bedfb8f235129df
tutorialcreation <tutorialcreation81@gmail.com>
2022-05-26 17:47:22 +0300

    added downloadable sales predictions

--- dashboard/predictions.py
+++ dashboard/predictions.py
@@ -4,12 +4,15 @@ import pickle
 import streamlit as st
 from io import StringIO
 
+@st.cache
+def convert_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv().encode('utf-8')
 
 
 
-def predict(*args,**kwargs):
-
-    pred_arr=np.array([kwargs])
+def predict(**kwargs):
+    pred_arr=np.array(list(kwargs.values()))
     preds=pred_arr.reshape(1,-1)
     preds=preds.astype(float)
     # model_prediction=lr_model.predict(preds)
@@ -20,24 +23,22 @@ def make_prediction(page=None):
     if st.session_state.page_select == page:
         st.subheader('Regressing Forecasts')
         if st.checkbox("Do you wish to upload file in order to make batch predictions,\
-            if not then please proceed to make a prediction"):
+            if not then please proceed to make a single sales prediction"):
             uploaded_file = st.file_uploader("Choose a file")
             if uploaded_file is not None:
-                # To read file as bytes:
-                bytes_data = uploaded_file.getvalue()
-                st.write(bytes_data)
-
-                # To convert to a string based IO:
-                stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-                st.write(stringio)
-
-                # To read file as string:
-                string_data = stringio.read()
-                st.write(string_data)
-
                 # Can be used wherever a "file-like" object is accepted:
                 dataframe = pd.read_csv(uploaded_file)
-                st.write(dataframe)
+                if st.button("Predict"):
+                    st.dataframe(dataframe.head())
+                    st.success("Predicted successfully")
+                    csv = convert_df(dataframe)
+
+                    st.download_button(
+                        label="Download predictions",
+                        data=csv,
+                        file_name='predictions.csv',
+                        mime='text/csv',
+                    )
         else:
             params = {
                 'var_1':st.text_input("Variable 1"),
@@ -48,9 +49,9 @@ def make_prediction(page=None):
             }
             if st.button("Predict"):
                 prediction=predict(**params)
-            st.success("The prediction by Model :{}".
-                        format(prediction))
-                
+                st.success("The prediction by Model :{}".
+                            format(prediction.shape))
+                    
 
         
     
