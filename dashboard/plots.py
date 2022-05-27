d0e84f9ed7205d2d29b7c7aaa82dcf6ac0346f29
tutorialcreation <tutorialcreation81@gmail.com>
2022-05-27 12:25:35 +0300

    added plots to dashboard

--- dashboard/plots.py
+++ dashboard/plots.py
@@ -1,7 +1,17 @@
 import streamlit as st
-
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scripts.database import DBOps
+sns.set()
 
 def view_predictions(page=None,results=None):
     if st.session_state.page_select == page:
         st.subheader('Predictions Chart')
+        db = DBOps(is_online=True)
+        df = pd.read_sql('select * from pharmaceuticalData',db.get_engine())
+        df.set_index('Date',inplace=True)
+        fig = plt.figure(figsize=(10, 4))
+        sns.lineplot(df.index, y=df['Sales'], hue=df['Customers']);
+        st.pyplot(fig)
         
