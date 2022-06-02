3fed8345e848ecc770424b05b5feffcb78a0dd84
Biruk Getaneh <bkgetmom@gmail.com>
2022-06-02 10:30:43 +0300

    remove log message

--- scripts/clean.py
+++ scripts/clean.py
@@ -514,7 +514,7 @@ class Clean:
                     while line:
                         text.append(line)
                         line = fp.readline()
-                    logger.info("successfully read file")
+            # logger.info("successfully read file")
         except FileNotFoundError as e:
             logger.error(e)
 
