34e9ef2e038d1e42871e84b616792f9df83b1f41
tutorialcreation <tutorialcreation81@gmail.com>
2022-06-03 13:12:29 +0300

    removed the decode batch prediction and placed it in the utils

--- scripts/deep_learner.py
+++ scripts/deep_learner.py
@@ -251,19 +251,7 @@ class DeepLearn:
         model.compile(optimizer=opt, loss=self.CTCLoss)
         return model
 
-    def decode_batch_predictions(self,pred,alphabet):
-        input_len = np.ones(pred.shape[0]) * pred.shape[1]
-        # Use greedy search. For complex tasks, you can use beam search
-        results = keras.backend.ctc_decode(pred, input_length=input_len, greedy=True)[0][0]
-        # Iterate over the results and get back the text
-        output_text = []
-        cleaner = Clean()
-        vocabs = cleaner.vocab(alphabet)
-        _,num_to_char = vocabs
-        for result in results:
-            result = tf.strings.reduce_join(num_to_char(result)).numpy().decode("utf-8")
-            output_text.append(result)
-        return output_text
+    
 
     
 if __name__=='__main__':
