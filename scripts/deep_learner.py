57551b5016ae04115d4c7242cccffa51052cde5b
tutorialcreation <tutorialcreation81@gmail.com>
2022-06-03 12:38:07 +0300

    changed rnn_layers and rnn_units

--- scripts/deep_learner.py
+++ scripts/deep_learner.py
@@ -191,7 +191,7 @@ class DeepLearn:
 
         
 
-    def build_asr_model(self,input_dim, output_dim, rnn_layers=1, rnn_units=1):
+    def build_asr_model(self,input_dim, output_dim, rnn_layers=5, rnn_units=128):
         """Model similar to DeepSpeech2."""
         # Model's input
         input_spectrogram = layers.Input((None, input_dim), name="input")
