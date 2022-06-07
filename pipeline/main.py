c5e233b303d1eff90936e21399792867abc2aeb3
tutorialcreation <tutorialcreation81@gmail.com>
2022-06-07 05:57:39 +0300

    added more configurations to mlflow

--- pipeline/main.py
+++ pipeline/main.py
@@ -1,14 +1,15 @@
 import mlflow
 import click
+import yaml
 uri = 'sqlite:///data_science.db'
 
-def _run(entrypoint, parameters={}, source_version=None, use_cache=True):
+def _run(entrypoint, parameters={}, params=None, source_version=None, use_cache=True):
     #existing_run = _already_ran(entrypoint, parameters, source_version)
     #if use_cache and existing_run:
     #    print("Found existing run for entrypoint=%s and parameters=%s" % (entrypoint, parameters))
      #   return existing_run
     print("Launching new run for entrypoint=%s and parameters=%s" % (entrypoint,parameters))
-    submitted_run = mlflow.run(".", entrypoint, **parameters)
+    submitted_run = mlflow.run(".", entrypoint, parameters=params,**parameters)
     return submitted_run
 
 
@@ -17,7 +18,15 @@ def workflow():
     with mlflow.start_run(run_name ="audio_pipeline") as active_run:
         mlflow.set_experiment("audiopipelines")
         mlflow.set_tag("mlflow.runName", "audio_pipeline")
-        _run("audio-deep-learner",{"backend":"local","use_conda":False})
+        
+        with open(r'params.yaml') as file:
+            documents = yaml.full_load(file)
+        
+        for item, doc in documents.items():
+            if item == 'model':
+                params_ = doc
+        _run("audio-deep-learner",{"backend":"local","use_conda":False},
+                            params=params_)
         
         
 if __name__=="__main__":
