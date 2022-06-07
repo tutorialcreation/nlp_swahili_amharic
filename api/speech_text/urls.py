e13c1e84cfbdd4a541afaa2e8a9e039d83fdb452
tutorialcreation <tutorialcreation81@gmail.com>
2022-06-07 03:58:24 +0300

    added an endpoint for fetching the audio

--- api/speech_text/urls.py
+++ api/speech_text/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
+from . import views
 app_name='stt'
 
 urlpatterns=[
-
+    path('create/',views.FetchAudio,name='fetch'),
 ]
--- api/speech_text/views.py
+++ api/speech_text/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.core.exceptions import ObjectDoesNotExist
+from .models import Audio
 # Create your views here.
+class FetchAudio(APIView):
+    def post(self,request,*args,**kwargs):
+        try:
+            audio = request.data.get('audio')
+        except ObjectDoesNotExist as e:
+            return Response({'error':e})
+        
+        Audio.objects.create(audio_file=audio)
+        return Response(
+            {
+                'data':'succesfully passed audio'
+            },
+            status=status.HTTP_201_CREATED)
