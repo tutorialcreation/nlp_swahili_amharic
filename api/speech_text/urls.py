aee76b183206bae696659a978368211b5a4ef2d1
tutorialcreation <tutorialcreation81@gmail.com>
2022-06-09 15:14:55 +0300

    added constants file

--- api/speech_text/urls.py
+++ api/speech_text/urls.py
@@ -12,4 +12,5 @@ urlpatterns=[
     path('lang/',views.FetchLanguage.as_view(),name='lang'),
     path('predict/',views.PredictView.as_view(),name='predict'),
     path('sr/',views.SPR.as_view(),name='spr'),
+    path('sta/',views.Stt.as_view(),name='sta'),
 ]
